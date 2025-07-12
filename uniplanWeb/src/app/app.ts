e804f4764b2635b950ed9a3037ade90487bd32ef
gslavchev <galinslavchev044@gmail.com>
2025-07-12 23:06:51 +0300

    Adding Angular Material to the project

--- uniplanWeb/src/app/app.ts
+++ uniplanWeb/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatSlideToggleModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
