9f6796eb20464cd3dc395599986231e114b6caba
Aleksandar <knifer0102@gmail.com>
2026-06-17 11:54:03 +0300

    Add lazy-loaded child routes (home, faculty, major, student) to app.routes.ts   Create HomePanel, FacultyPanel, MajorPanel, StudentPanel routed components   Move filter state from MainPanel into MajorPanel and StudentPanel   Replace <app-main-panel> with <router-outlet> in LayoutComponent   Replace ViewService click handlers with [routerLink] and routerLinkActive in NavmenuComponent   Move layout styles to layout-component.scss, shared panel styles to styles.scss   Delete MainPanel .html,.scss,.spec.ts,.ts   Delete table .html,.scss,.spec.ts,.ts   Deleted view.service.ts

--- uniplanWeb/src/app/app.routes.ts
+++ uniplanWeb/src/app/app.routes.ts
@@ -2,5 +2,31 @@ import { Routes } from '@angular/router';
 import { LayoutComponent } from './layout-component/layout-component';
 
 export const routes: Routes = [
-    {path: "" , component: LayoutComponent}
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      { path: '', redirectTo: 'home', pathMatch: 'full' },
+      {
+        path: 'home',
+        loadComponent: () =>
+          import('./features/home/home-panel/home-panel').then(m => m.HomePanel),
+      },
+      {
+        path: 'faculty',
+        loadComponent: () =>
+          import('./features/faculty/faculty-panel/faculty-panel').then(m => m.FacultyPanel),
+      },
+      {
+        path: 'major',
+        loadComponent: () =>
+          import('./features/major/major-panel/major-panel').then(m => m.MajorPanel),
+      },
+      {
+        path: 'student',
+        loadComponent: () =>
+          import('./features/student/student-panel/student-panel').then(m => m.StudentPanel),
+      },
+    ],
+  },
 ];
---
+++ uniplanWeb/src/app/features/home/home-panel/home-panel.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-panel',
+  standalone: true,
+  template: `<div class="component-panel home"><h1>Main!</h1></div>`,
+})
+export class HomePanel {}
