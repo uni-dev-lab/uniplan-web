f496cafdf2145d77d0f1ff7cba95b211be1cddeb
Aleksandar <knifer0102@gmail.com>
2026-06-17 11:54:03 +0300

    Add lazy-loaded child routes (home, faculty, major, student) to app.routes.ts   Create HomePanel, FacultyPanel, MajorPanel, StudentPanel routed components   Move filter state from MainPanel into MajorPanel and StudentPanel   Replace <app-main-panel> with <router-outlet> in LayoutComponent   Replace ViewService click handlers with [routerLink] and routerLinkActive in NavmenuComponent   Move layout styles to layout-component.scss, shared panel styles to styles.scss   Delete MainPanel .html,.scss,.spec.ts,.ts   Delete table .html,.scss,.spec.ts,.ts   Deleted view.service.ts

--- uniplanWeb/src/app/core/shared/navmenu-component/navmenu-component.ts
+++ uniplanWeb/src/app/core/shared/navmenu-component/navmenu-component.ts
@@ -1,13 +1,13 @@
 import { Component, HostListener, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
-import { ViewService } from '../main-panel/view.service';
 import { LoginAuthService } from '../../../services/login-auth-service';
 
 @Component({
   selector: 'app-navmenu-component',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterLinkActive],
   templateUrl: './navmenu-component.html',
   changeDetection: ChangeDetectionStrategy.Eager,
   styleUrls: ['./navmenu-component.scss'],
@@ -16,10 +16,7 @@ export class NavmenuComponent implements OnInit {
   isSidebarCollapsed = false;
   isMobileView = window.innerWidth <= 768;
 
-  constructor(
-    public authService: LoginAuthService,
-    public viewService: ViewService
-  ) {}
+  constructor(public authService: LoginAuthService) {}
 
   ngOnInit(): void {
     this.checkViewport();
@@ -42,21 +39,5 @@ export class NavmenuComponent implements OnInit {
 
   toggleSidebar(): void {
     this.isSidebarCollapsed = !this.isSidebarCollapsed;
-  }
-
-  onHomeClick(): void {
-    this.viewService.setView('home');
-  }
-
-  onFacultyClick(): void {
-    this.viewService.setView('faculty');
-  }
-
-  onMajorClick(): void {
-    this.viewService.setView('major');
-  }
-
-  onStudentClick(): void {
-    this.viewService.setView('student');
   }
 }
