26fa631e8ffb80a9220c4fcbf3755948385a3b2e
Ivcho <stu2401261003@uni-plovdiv.bg>
2026-06-18 12:08:41 +0300

    Urls extracted into tow files for the base url and the specific ones
    # Conflicts:
    #	uniplanWeb/src/app/features/faculty/faculty-service.ts
    #	uniplanWeb/src/app/features/major/major-service.ts
    #	uniplanWeb/src/app/features/university/university-service.ts

---
+++ uniplanWeb/src/app/config/endpoints.ts
@@ -0,0 +1,8 @@
+import { environment } from '../../environments/environment';
+
+export const API_ENDPOINTS = {
+  universities: `${environment.baseUrl}/universities`,
+  courses: `${environment.baseUrl}/courses`,
+  faculties: `${environment.baseUrl}/faculties`,
+  majors: `${environment.baseUrl}/majors`,
+};
