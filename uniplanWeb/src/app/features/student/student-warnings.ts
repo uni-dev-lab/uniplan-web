ac9ec743a11ec5940c91212155ba87d824acba0d
constantine0621 <dosevkonstantin3@gmail.com>
2026-06-24 11:39:01 +0300

    adding shared warning messages for Add and Edit forms

---
+++ uniplanWeb/src/app/features/student/student-warnings.ts
@@ -0,0 +1,9 @@
+export const StudentWarnings = {
+  firstNameMissing: 'Моля въведете първо име на студента.',
+  lastNameMissing: 'Моля въведете фамилия на студента',
+  facultyNumberMissing: 'Моля въведете факултетен номер',
+  majorMissing: 'Моля изберете специалност',
+  courseSubtypeMissing: 'Моля изберете форма на обучение',
+  courseTypeMissing: 'Моля изберете квалификационна степен',
+  courseYearMissing: 'Моля изберете курс',
+};
