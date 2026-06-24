dffef943b0f7a84e98b092ae65de33105cc97dbc
Djesika Vacheva <stu2401261031@uni-plovdiv.bg>
2026-06-24 11:41:53 +0300

    Use localStorage as login state source

--- uniplanWeb/src/app/services/login-auth-service.ts
+++ uniplanWeb/src/app/services/login-auth-service.ts
@@ -4,11 +4,6 @@ import { Injectable } from '@angular/core';
   providedIn: 'root',
 })
 export class LoginAuthService {
-  private loggedIn = false;
-
-  public constructor() {
-    this.loggedIn = !!localStorage.getItem('user');
-  }
 
   public login(username: string, password: string): boolean {
     if (!username || !password) {
@@ -16,18 +11,16 @@ export class LoginAuthService {
     }
 
     localStorage.setItem('user', username);
-    this.loggedIn = true;
 
     return true;
   }
 
   public isLoggedIn(): boolean {
-    return this.loggedIn;
+    return !!localStorage.getItem('user');
   }
 
   public logout(): void {
     localStorage.removeItem('user');
-    this.loggedIn = false;
   }
 
   public getUsername(): string {
