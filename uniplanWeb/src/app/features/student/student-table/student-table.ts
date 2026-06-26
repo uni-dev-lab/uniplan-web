c15562c8be3beb922c7adb771579ce75b846e31d
constantine0621 <dosevkonstantin3@gmail.com>
2026-06-26 10:29:28 +0300

    feat(student-table): emit subtypesLoaded event and clean up unused code

--- uniplanWeb/src/app/features/student/student-table/student-table.ts
+++ uniplanWeb/src/app/features/student/student-table/student-table.ts
@@ -5,7 +5,8 @@ import {
   OnChanges,
   OnInit,
   ChangeDetectionStrategy,
-  inject
+  inject,
+  output
 } from '@angular/core';
 import { StudentElm } from '../../../core/interfaces/student-elm';
 import { TranslatePipe } from '@ngx-translate/core';
@@ -33,6 +34,8 @@ export class StudentTable implements OnInit, OnChanges {
   @Input() subtype = '';
 
   @Input() subtypes: string[] = [];
+
+  subtypesLoaded = output<string[]>();
 
   displayedColumns: string[] = [
     'position',
@@ -64,6 +67,7 @@ export class StudentTable implements OnInit, OnChanges {
       next: (students) => {
         this.originalData = students;
         this.subtypes = StudentTable.getFilterOptions(students).subtypes;
+        this.subtypesLoaded.emit(this.subtypes);
         this.applyFilters();
       },
       error: () => {
