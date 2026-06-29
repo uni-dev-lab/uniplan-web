eb1060a0587665381b7256fd3400a4060e024169
constantine0621 <dosevkonstantin3@gmail.com>
2026-06-29 10:57:10 +0300

    fix(course-service): revert apiUrl to local development URL

--- uniplanWeb/src/app/features/course/course-service.ts
+++ uniplanWeb/src/app/features/course/course-service.ts
@@ -2,13 +2,12 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CourseElm } from '../../core/interfaces/course-elm';
-import { environment } from '../../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
-  private apiUrl = environment.testUrl.coursesUrl;
+  private apiUrl = 'http://localhost:8080/api/courses';
   private http = inject(HttpClient);
 
   getCoursesByMajorId(majorId: string): Observable<CourseElm[]> {
