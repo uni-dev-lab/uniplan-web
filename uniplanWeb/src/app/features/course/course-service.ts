001c74b85f5392c96b42b502732ac3efaa559fd3
constantine0621 <dosevkonstantin3@gmail.com>
2026-06-30 14:42:41 +0300

    refactor: update CourseService to use API_ENDPOINTS for URL management

--- uniplanWeb/src/app/features/course/course-service.ts
+++ uniplanWeb/src/app/features/course/course-service.ts
@@ -2,16 +2,16 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CourseElm } from '../../core/interfaces/course-elm';
-import { environment } from '../../../environments/environments';
+import { API_ENDPOINTS } from '../../config/endpoints';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CourseService {
-  private apiUrl = environment.testUrl.coursesUrl;
+  private apiUrl = API_ENDPOINTS.courses
   private http = inject(HttpClient);
 
   getCoursesByMajorId(majorId: string): Observable<CourseElm[]> {
-    return this.http.get<CourseElm[]>(`${this.apiUrl}/major/${majorId}`);
+    return this.http.get<CourseElm[]>(`${API_ENDPOINTS.faculties}/major/${majorId}`);
   }
 }
