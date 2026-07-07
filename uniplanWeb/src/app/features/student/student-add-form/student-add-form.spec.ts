e89f9a9a083f8bd1bdc474cd1245a92ff13b2b2b
constantine0621 <dosevkonstantin3@gmail.com>
2026-07-07 18:24:12 +0300

    improve student-add-form test setup and mock services

--- uniplanWeb/src/app/features/student/student-add-form/student-add-form.spec.ts
+++ uniplanWeb/src/app/features/student/student-add-form/student-add-form.spec.ts
@@ -1,20 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 import { translateTestingProviders } from '@testing/translate-testing';
-import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
-
+import { MatDialogRef } from '@angular/material/dialog';
+import { MajorService } from '../../major/major-service';
+import { CourseService } from '../../course/course-service';
+import { StudentService } from '../student-service';
 import { StudentAddForm } from './student-add-form';
 
 describe('StudentAddForm', () => {
   let component: StudentAddForm;
   let fixture: ComponentFixture<StudentAddForm>;
+  let majorServiceSpy: jasmine.SpyObj<MajorService>;
+  let courseServiceSpy: jasmine.SpyObj<CourseService>;
+  let studentServiceSpy: jasmine.SpyObj<StudentService>;
 
   beforeEach(async () => {
+    majorServiceSpy = jasmine.createSpyObj('MajorService', ['getMajors']);
+    courseServiceSpy = jasmine.createSpyObj('CourseService', ['getCoursesByMajorId']);
+    studentServiceSpy = jasmine.createSpyObj('StudentService', ['createStudent']);
+
+    majorServiceSpy.getMajors.and.returnValue(of([]));
+
     await TestBed.configureTestingModule({
       imports: [StudentAddForm],
       providers: [
         ...translateTestingProviders,
-        { provide: MatDialogRef, useValue: {} },
-        { provide: MAT_DIALOG_DATA, useValue: {} },
+        { provide: MatDialogRef, useValue: { close: jasmine.createSpy('close') } },
+        { provide: MajorService, useValue: majorServiceSpy },
+        { provide: CourseService, useValue: courseServiceSpy },
+        { provide: StudentService, useValue: studentServiceSpy },
       ],
     }).compileComponents();
 
